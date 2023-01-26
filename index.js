8e051621a47bf0ec01762ef1987a0ac33341820e
ubrong <ubrong@qq.com>
2023-01-26 10:22:38 +0800

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'Hello Vercel-9999999';
+});
+
+app.listen(3008, () => {
+    console.log('3008项目启动')
+});
